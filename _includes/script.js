71d9b47877f73103cb55094f90c484e22d053858
Baki <bakinazikk@gmail.com>
2026-09-13 07:59:53 +0300

    Update repository

--- _includes/script.js
+++ _includes/script.js
@@ -785,9 +785,6 @@
       img.addEventListener("click", function () {
         open(img.closest(".photo-item"), true);
       });
-
-      function markLoaded() { img.classList.add("is-loaded"); }
-      if (img.complete) markLoaded(); else img.addEventListener("load", markLoaded);
     });
 
     lightbox.addEventListener("click", function () {
